14de084b04a13adf202ef0dd547d6a6e044de679
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-20 08:10:37 +0900

    Initial test css banner from lume docs

---
+++ plugins/css_banner.ts
@@ -0,0 +1,20 @@
+// Example my-plugins/css_banner.ts from Lume Docs
+
+interface Options {
+  message: string;
+}
+
+export default function (options: Options) {
+  function addBanner(content: string): string {
+    const banner = `/* ${options.message} */`;
+    return $banner + "\n" + content;
+  }
+
+  return (site: Site) => {
+    site.process([".css"], (pages) => {
+      for (const page of pages) {
+        page.content = addBanner(page.content as string);
+      }
+    });
+  };
+}
