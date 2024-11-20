2ca73ad6ac4dc2d1c1de7de2e6822db6a03b3a76
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-20 10:10:36 +0900

    Remove redundant name, thx @oscarotero

--- plugins/shuffle.ts
+++ plugins/shuffle.ts
@@ -32,7 +32,7 @@ export const defaults: Options = {
 export function shuffle(userOptions?: Options) {
   const options = merge(defaults, userOptions);
   return (site: Site) => {
-    site.filter(options.names.shuffle!, "shuffle", <T>(array: T[] = []) => {
+    site.filter(options.names.shuffle!, <T>(array: T[] = []) => {
         for (let i = array.length - 1; i >= 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
           [array[i], array[j]] = [array[j], array[i]];
