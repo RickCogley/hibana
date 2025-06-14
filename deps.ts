805014c446a2c5a5b98d365ec3819d82e1f94cf1
James R. Cogley <rick.cogley@esolia.co.jp>
2025-06-14 20:34:14 +0900

    Bump hibana version, update most deps to use jsr

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 // Centralized dependency exports for hibana
 
-export { DOMParser } from "https://deno.land/x/deno_dom@0.1.48/deno-dom-wasm.ts";
-export * as assert from "https://deno.land/std@0.203.0/assert/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
+export { DOMParser } from "jsr:@b-fuze/deno-dom";
+export * as assert from "jsr:@std/assert";
+export { assertEquals } from "jsr:@std/assert";
 export * as lume from "https://deno.land/x/lume@v3.0.4/mod.ts";
--- mod.ts
+++ mod.ts
@@ -22,11 +22,11 @@
  *   "lume/": "https://deno.land/x/lume@v3.0.3/",
  *   "lume/cms/": "https://cdn.jsdelivr.net/gh/lumeland/cms@0.12.0/",
  *   "lume/jsx-runtime": "https://deno.land/x/ssx@v0.1.10/jsx-runtime.ts",
- *   "hibana/": "https://deno.land/x/hibana/v1.0.16/"
+ *   "hibana/": "https://deno.land/x/hibana/v1.0.17/"
  * },...
  * 
  * // Github Direct: 
- * "hibana/": "https://raw.githubusercontent.com/RickCogley/hibana/v1.0.16/"
+ * "hibana/": "https://raw.githubusercontent.com/RickCogley/hibana/v1.0.17/"
  * 
  * // Then import it in your Lume config:
  * import { cssBanner, shuffle, deferPagefind, externalLinksIcon } from "hibana/mod.ts";
