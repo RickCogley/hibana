28e9f5ac6577b90187b5be22804a6fb680743b7a
James R. Cogley <rick.cogley@esolia.co.jp>
2025-06-16 19:44:51 +0900

    Adds jsdoc
    It's jsdoc kind of, with some tags that deno doc expects..

--- mod.ts
+++ mod.ts
@@ -1,35 +1,49 @@
 /**
- * @file This is the main entry point for the 'hibana' Lume plugin, processor and utility library.
- * @module hibana
- * @author Rick Cogley
- * @license MIT 
- * @link https://github.com/RickCogley/hibana - The official Git repository for this project.
- *
- * @description
- * This module re-exports various Lume plugins and utility functions
- * designed to enhance Lume static site generation projects.
+ * The `hibana` library re-exports various Lume helper plugins and utility functions designed to enhance Lume static site generation projects.
+ * 
+ * | Hibana | Specifications |
+ * | ------- | ------- |
+ * | **Version** | 1.0.18 |
+ * | **Repository** | {@link https://github.com/RickCogley/hibana} |
+ * | **License** | MIT |
+ * | **Author** | Rick Cogley |
  *
  * It provides:
- * - `cssBanner`: A plugin to add a comment banner to CSS files.
- * - `shuffle`: A plugin to add a shuffle filter for arrays.
- * - `deferPagefind`: A processor to defer Pagefind CSS and JS loading.
- * - `externalLinksIcon`: A processor to add external link icons to `target="_blank"` links.
- * - `loadVendorScript` and `trapFocus`: General DOM utility functions.
+ * - {@link cssBanner} - A plugin to add a comment banner to CSS files.
+ * - {@link shuffle} - A plugin to add a shuffle filter for arrays.
+ * - {@link deferPagefind} - A processor to defer Pagefind CSS and JS loading.
+ * - {@link externalLinksIcon} - A processor to add external link icons to `target="_blank"` links.
+ * - {@link loadVendorScript}, {@link trapFocus} - General DOM utility functions.
  * 
- * @example
- * // In Lume's deno.json, you can add it to your import map:
+ * 
+ * @see {@link http://github.com|GitHub}
+ * @see {@link https://github.com/RickCogley/hibana} official Git repository for this project.
+ * @tags lume, plugin, processor, utility, static-site-generator
+ * @file This file is the main entry point for the 'hibana' Lume plugin, processor and utility library.
+ * @module hibana
+ * @author Rick Cogley
+ * @license MIT
+ *
+ * 
+ * @example Add `hibana` to your Lume project's `deno.json` import map:
+ * ```ts
+ * ...
  * "imports": {
  *   "lume/": "https://deno.land/x/lume@v3.0.3/",
  *   "lume/cms/": "https://cdn.jsdelivr.net/gh/lumeland/cms@0.12.0/",
  *   "lume/jsx-runtime": "https://deno.land/x/ssx@v0.1.10/jsx-runtime.ts",
  *   "hibana/": "https://deno.land/x/hibana/v1.0.18/"
  * },...
- * 
- * // Github Direct: 
+ * ```
+ * @example Or you can import `hibana` directly from GitHub:
+ * ```ts 
  * "hibana/": "https://raw.githubusercontent.com/RickCogley/hibana/v1.0.18/"
+ * ```
  * 
- * // Then import it in your Lume config:
+ * @example Then import `hibana` into your Lume `_config.ts`:
+ * ```ts
  * import { cssBanner, shuffle, deferPagefind, externalLinksIcon } from "hibana/mod.ts";
+ * ```
  */
 
 // Plugins
