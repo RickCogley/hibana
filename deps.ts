65ae108b7349fe66c705f0457df3efa56bf8d315
James R. Cogley <rick.cogley@esolia.co.jp>
2025-11-21 06:38:42 +0900

    build: upgrade Lume from v3.0.4 to v3.1.2

--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@
 export { DOMParser } from "jsr:@b-fuze/deno-dom";
 export * as assert from "jsr:@std/assert";
 export { assertEquals } from "jsr:@std/assert";
-export * as lume from "https://deno.land/x/lume@v3.0.4/mod.ts";
+export * as lume from "https://deno.land/x/lume@v3.1.2/mod.ts";
